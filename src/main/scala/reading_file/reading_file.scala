29e69dd4570fcf3ab6bc5b9e3baf79047a26311a
Karim Derouiche <karimderouiche@MacBook-Air-de-Karim.local>
2021-10-12 22:10:14 +0200

    reading in txt file

--- src/main/scala/reading_file/reading_file.scala
+++ src/main/scala/reading_file/reading_file.scala
@@ -1,5 +1,15 @@
 package reading_file
+import scala.io.Source
 
-object reading_file {
-  println("Hello test")
+object reading_file extends App {
+
+  val filename = "src/main/scala/reading_file/data.txt"
+  val fichier = Source.fromFile(filename).getLines()
+
+  for (line <- fichier) {
+    println(line)
+  }
+
+
+
 }
