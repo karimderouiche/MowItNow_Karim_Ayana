4f51b3ca074f1a8780a5761c03a8b9675cc07b8b
Karim Derouiche <karimderouiche@MacBook-Air-de-Karim.local>
2021-10-13 16:24:25 +0200

    Ajout du dossier

---
+++ src/main/scala/parserFile/Parser.scala
@@ -0,0 +1,35 @@
+package parserFile
+import scala.io.Source
+
+object Parser extends App {
+
+  val filename = "src/main/scala/reading_file/data.txt"
+  val fichier = Source.fromFile(filename).getLines()
+
+  val coordonnees = fichier.slice(0,1).toList.headOption.mkString.split(" ")
+
+  var fichier_list = fichier.toList
+  println(fichier_list)
+
+  val size_fichier = fichier_list.size
+
+
+  println("la taille du fichier est de ")
+  println(size_fichier)
+
+  println("Les coordonnées du point supérieur du jardin sont ")
+  println(coordonnees.mkString(","))
+
+  // On crée les array de positions et d'instructions
+  val positions = fichier_list.zipWithIndex.filter(_._2 % 2 == 0).map(_._1)
+  val instructions = fichier_list.zipWithIndex.filter(_._2 % 2 == 1).map(_._1)
+
+  // Il faut peut être gérer le cas où le nombre de ligne n'est pas pair
+
+  println("Liste des positions:")
+  println(positions)
+
+  println("Liste des instructions:")
+  println(instructions)
+
+}
