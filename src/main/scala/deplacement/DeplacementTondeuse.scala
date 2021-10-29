1480fa031c103008b6498226edba80bd4176f34c
Karim Derouiche <karimderouiche@MacBook-Air-de-Karim.local>
2021-10-29 22:13:29 +0200

    Coorections des fonctions de déplacement

--- src/main/scala/deplacement/DeplacementTondeuse.scala
+++ src/main/scala/deplacement/DeplacementTondeuse.scala
@@ -12,32 +12,34 @@ class DeplacementTondeuse {
       instruction match {
         case "A" => tondeuse.coordonnees.orientation match
         {
-          case "N" => tondeuse.coordonnees.copy(y = tondeuse.coordonnees.y + 1)
-          case "W" => tondeuse.coordonnees.copy(x = tondeuse.coordonnees.x - 1)
-          case "S" => tondeuse.coordonnees.copy(y = tondeuse.coordonnees.y - 1)
-          case "E" => tondeuse.coordonnees.copy(x = tondeuse.coordonnees.x + 1)
+          case "N" => tondeuse.coordonnees.y = tondeuse.coordonnees.y + 1
+          case "W" => tondeuse.coordonnees.x = tondeuse.coordonnees.x - 1
+          case "S" => tondeuse.coordonnees.y = tondeuse.coordonnees.y - 1
+          case "E" => tondeuse.coordonnees.x = tondeuse.coordonnees.x + 1
           case _ => println("Erreur, une orientation n'est pas comprise")
         }
+        println(tondeuse.coordonnees.x)
+        println(tondeuse.coordonnees.orientation)
         case "G" => tondeuse.coordonnees.orientation match
         {
-          case "N" => tondeuse.coordonnees.copy(orientation = "W")
-          case "W" => tondeuse.coordonnees.copy(orientation = "S")
-          case "S" => tondeuse.coordonnees.copy(orientation = "E")
-          case "E" => tondeuse.coordonnees.copy(orientation = "N")
+          case "N" => tondeuse.coordonnees.orientation = "W"
+          case "W" => tondeuse.coordonnees.orientation = "S"
+          case "S" => tondeuse.coordonnees.orientation = "E"
+          case "E" => tondeuse.coordonnees.orientation = "N"
           case _ => println("Erreur, une orientation n'est pas comprise")
         }
         case "D" => tondeuse.coordonnees.orientation match
         {
-          case "N" => tondeuse.coordonnees.copy(orientation = "E")
-          case "W" => tondeuse.coordonnees.copy(orientation = "S")
-          case "S" => tondeuse.coordonnees.copy(orientation = "W")
-          case "E" => tondeuse.coordonnees.copy(orientation = "N")
+          case "N" => tondeuse.coordonnees.orientation = "E"
+          case "W" => tondeuse.coordonnees.orientation = "N"
+          case "S" => tondeuse.coordonnees.orientation = "W"
+          case "E" => tondeuse.coordonnees.orientation = "S"
           case _ => println("Erreur, une orientation n'est pas comprise")
         }
         case _ => println("Erreur, une instruction n'est pas comprise")
       }
     }
-
+    println("Tondeuse " + tondeuse.coordonnees.x + " " + + tondeuse.coordonnees.y + " " + tondeuse.coordonnees.orientation)
 
   }
 
