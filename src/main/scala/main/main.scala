7a71d101175e40ddc9f44f68eac9bfa3163d3208
Karim Derouiche <karimderouiche@MacBook-Air-de-Karim.local>
2021-10-29 23:22:56 +0200

    création du fichier main

--- src/main/scala/main/main.scala
+++ src/main/scala/main/main.scala
@@ -1,5 +1,13 @@
 package main
 
-object main {
+import parser.parserFile
+import deplacement.DeplacementTondeuse
+import structure.{Pelouse, Tondeuse}
+import scala.reflect.io.File
+
+
+object main extends App {
+
+
 
 }
