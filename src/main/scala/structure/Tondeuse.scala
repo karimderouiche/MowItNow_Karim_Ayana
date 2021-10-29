d0a33d7448b3d0d61f5454956e88f4ac227bdd23
Karim Derouiche <karimderouiche@MacBook-Air-de-Karim.local>
2021-10-29 22:16:49 +0200

    Modification de la classe tondeuse

--- src/main/scala/structure/Tondeuse.scala
+++ src/main/scala/structure/Tondeuse.scala
@@ -4,7 +4,7 @@ case class Tondeuse(coordonnees: Coordonnees, pelouse: Pelouse){
 
   try {
     assert(coordonnees.x < pelouse.coinSuperieur.x && coordonnees.y < pelouse.coinSuperieur.y &&
-      coordonnees.x > 0 && coordonnees.y > 0)
+      coordonnees.x >= 0 && coordonnees.y >= 0)
   } catch {
     case _ => println("La tondeuse est sortie de la pelouse !")
   }
